6bfa23a3cb7dc59c916ccef2834430d97c2983c0
Viktor Trako <viktor.trako@btinternet.com>
2013-09-24 12:42:36 +0100

    errors should all be in json format

--- test/run.js
+++ test/run.js
@@ -1,7 +1,8 @@
 var config = require('../config').config;
 if(config.test_jwt_claim_set_iss == '' || config.test_pem_file == '' || config.test_modelID == '') {
-  throw new Error('Provide test_jwt_claim_set_iss, test_pem_file, and test_modelID in config/config.js for these tests to run \n'
-  );
+  throw new Error({
+    message:'Provide test_jwt_claim_set_iss, test_pem_file, and test_modelID in config/config.js for these tests to run'
+    });
 } else {
   require('urun')(__dirname, {include: /test[^\/]+\.js$/});
 }
